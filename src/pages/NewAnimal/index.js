591fece5b07b80e85db91fca5ee494342e9b58aa
Helder <helderafonsoss@hotmail.com>
2020-10-02 22:36:54 -0300

    NewAnimal: Criacao da pagina

---
+++ src/pages/NewAnimal/index.js
@@ -0,0 +1,464 @@
+import React, { useCallback, useState, useRef, useEffect } from 'react';
+import { Form } from '@unform/web';
+import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import * as Yup from 'yup';
+import { uuid } from 'uuidv4';
+import {
+  IoMdImages,
+  IoIosClose,
+  IoIosWater,
+  IoMdAdd,
+  IoIosEye,
+} from 'react-icons/io';
+
+import { useRegion } from '../../hooks/region';
+import api from '../../services/api';
+
+import {
+  Container,
+  Content,
+  Title,
+  FormContainer,
+  Select,
+  Select2,
+  Input,
+  Input2,
+  FileInput,
+  Thumbnails,
+  PhotoInfo,
+  Buttons,
+  Button,
+  Page,
+  Row,
+  InputsContainer,
+  InputContainer,
+  AddButton,
+  Checkbox,
+} from './styles';
+
+import getValidationErrors from '../../utils/getValidationErrors';
+
+const NewAnimal = () => {
+  const { region } = useRegion();
+  const history = useHistory();
+
+  const formRef = useRef(null);
+  const [page, setPage] = useState(1);
+  const [photos, setPhotos] = useState([]);
+  const [vaccinesInputs, setVaccinesInputs] = useState([]);
+  const [observationsInputs, setObservationsInputs] = useState([]);
+
+  const [allAnimals, setAllAnimals] = useState([]);
+  const [animalsOptions, setAnimalsOptions] = useState([]);
+  const [breedsOptions, setBreedsOptions] = useState([]);
+
+  useEffect(() => {
+    api.get('/breeds').then(response => {
+      setAllAnimals(response.data);
+    });
+  }, []);
+
+  const getPhotos = useCallback(() => {
+    let selectedPhotos = Array.from(
+      formRef.current.getFieldRef('photos').files,
+    );
+
+    // Deixa somente 10 fotos
+    selectedPhotos.splice(10);
+
+    // remove fotos duplicadas
+    selectedPhotos = photos.concat(
+      selectedPhotos.filter(item => photos.indexOf(item) < 0),
+    );
+    setPhotos(selectedPhotos);
+  }, [formRef, photos]);
+
+  const handleRemovePhoto = useCallback(
+    photoName => {
+      const index = photos.findIndex(photo => photo.name === photoName);
+      const selectedPhotos = Array.from(photos);
+      selectedPhotos.splice(index, 1);
+      setPhotos(selectedPhotos);
+    },
+    [photos],
+  );
+
+  const handleAddInput = useCallback(
+    input => {
+      switch (input) {
+        case 'vaccine':
+          setVaccinesInputs(vaccinesInputs.concat(uuid()));
+          break;
+        case 'observation':
+          setObservationsInputs(observationsInputs.concat(uuid()));
+          break;
+        default:
+          console.log('Valor invalido');
+          break;
+      }
+    },
+    [vaccinesInputs, observationsInputs],
+  );
+
+  const handleRemoveInput = useCallback(
+    (input, index) => {
+      switch (input) {
+        case 'vaccine':
+          setVaccinesInputs(vaccinesInputs.filter((value, i) => index !== i));
+          break;
+        case 'observation':
+          setObservationsInputs(
+            observationsInputs.filter((value, i) => index !== i),
+          );
+          break;
+        default:
+          console.log('Valor invalido');
+          break;
+      }
+    },
+    [observationsInputs, vaccinesInputs],
+  );
+
+  const handleNextPage = useCallback(() => {
+    if (page < 3) setPage(page + 1);
+  }, [page]);
+
+  const handlePreviusPage = useCallback(() => {
+    if (page > 1) setPage(page - 1);
+  }, [page]);
+
+  useEffect(() => {
+    setAnimalsOptions([...new Set(allAnimals.map(({ animal }) => animal))]);
+  }, [allAnimals]);
+
+  const handleShowBreed = useCallback(() => {
+    const selectedAnimal = formRef.current.getFieldValue('animal');
+
+    const breedOptions = allAnimals
+      .filter(({ animal }) => animal === selectedAnimal)
+      .map(({ breed }) => breed);
+
+    setBreedsOptions(breedOptions);
+  }, [allAnimals]);
+
+  const handleSubmit = useCallback(
+    async data => {
+      const page1 = Yup.object().shape({
+        category: Yup.string().required('Categoria é obrigatória'),
+        photos: Yup.array().min(3).max(10).required(),
+      });
+
+      const page2 = Yup.object().shape({
+        name: Yup.string().required('Digite o nome do animal'),
+        animal: Yup.string().required('Selecione o animal'),
+        breed: Yup.string().required('Selecione a raça'),
+        port: Yup.string()
+          .required()
+          .oneOf(['Pequeno', 'Médio', 'Grande'], 'Selecione o porte do animal'),
+        years_old: Yup.number().required('Digite a idade do animal'),
+      });
+
+      const page3 = Yup.object().shape({
+        vaccines: Yup.array().of(Yup.string()),
+        observations: Yup.array().of(Yup.string()),
+      });
+      try {
+        switch (page) {
+          case 1:
+            await page1.validate(
+              { ...data, photos },
+              {
+                abortEarly: false,
+              },
+            );
+            break;
+          case 2:
+            await page2.validate(data, {
+              abortEarly: false,
+            });
+            break;
+          case 3:
+            await page3.validate(
+              { ...data },
+              {
+                abortEarly: false,
+              },
+            );
+            // const files = fileRef.current.files;
+            const selectedAnimal = allAnimals.filter(
+              ({ animal, breed }) =>
+                animal === data.animal && breed === data.breed,
+            );
+            try {
+              // Limpa posicoes vazias do array ''
+              data.vaccines = data.vaccines.filter(value => value !== '');
+              data.observations = data.observations.filter(
+                value => value !== '',
+              );
+
+              const formData = new FormData();
+              formData.append('name', data.name);
+              formData.append('breed_id', selectedAnimal[0].id);
+              formData.append('genre', data.genre);
+              formData.append('port', data.port);
+              formData.append('years_old', data.years_old);
+              formData.append('pedigree', data.pedigree ? 'Sim' : 'Não');
+              formData.append('castrated', data.castrated ? 'Sim' : 'Não');
+              formData.append('category', data.category);
+              photos.map(photo => {
+                return formData.append('photos', photo);
+              });
+
+              // caso haja vacinas
+              if (data.vaccines.length > 0) {
+                formData.append('vaccines', data.vaccines.toString());
+              }
+
+              // caso haja observacoes
+              if (data.observations.length > 0) {
+                formData.append('observations', data.observations.toString());
+              }
+              await api.post(`/my/animals/${region.url_param}`, formData);
+
+              toast.info('O cadastro do animal foi realizado com sucesso!');
+
+              history.goBack();
+            } catch (err) {
+              if (err.isAxiosError) {
+                console.log(err.response.data.message);
+              }
+
+              toast.info(
+                'Ocorreu algum erro. Por favor, tente novamente mais tarde.',
+              );
+            }
+
+            return;
+          default:
+            break;
+        }
+
+        // Nao deu erro
+        handleNextPage();
+      } catch (err) {
+        console.log(err);
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current.setErrors(errors);
+
+          return;
+        }
+
+        toast.error('Ocorreu um erro ao fazer login, cheque as credenciais.', {
+          position: 'top-right',
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      }
+    },
+    [page, photos, handleNextPage, allAnimals, region.url_param, history],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <Title>Cadastrar Animal</Title>
+        <FormContainer>
+          <Form onSubmit={handleSubmit} ref={formRef}>
+            {/* Pagina 1 */}
+            <Page visible={page === 1}>
+              <InputContainer>
+                <Select
+                  name="category"
+                  title="Categoria"
+                  placeholder="Selecione a categoria"
+                  options={['Adoção', 'Desaparecido']}
+                />
+              </InputContainer>
+              <FileInput>
+                <Input
+                  name="photos"
+                  title="Fotos"
+                  type="file"
+                  onChange={getPhotos}
+                  multiple
+                  icon={IoMdImages}
+                  disabled={photos.length >= 10}
+                />
+                <Thumbnails>
+                  {photos.map(photo => (
+                    <PhotoInfo key={photo.name}>
+                      <div>
+                        <img
+                          src={URL.createObjectURL(photo)}
+                          alt={photo.name}
+                        />
+                        <span>{photo.name}</span>
+                      </div>
+                      <button
+                        type="button"
+                        onClick={() => handleRemovePhoto(photo.name)}
+                      >
+                        <IoIosClose size={32} />
+                      </button>
+                    </PhotoInfo>
+                  ))}
+                </Thumbnails>
+              </FileInput>
+
+              <Buttons>
+                <Button title="Proximo" buttonType="next" />
+              </Buttons>
+            </Page>
+
+            {/* Pagina 2 */}
+            <Page visible={page === 2}>
+              <InputsContainer>
+                <Row>
+                  <Select2
+                    name="animal"
+                    title="Animal"
+                    placeholder="Selecione a categoria"
+                    options={animalsOptions}
+                    onChange={handleShowBreed}
+                  />
+
+                  <Select2
+                    styled
+                    name="breed"
+                    title="Raça"
+                    placeholder="Selecione a categoria"
+                    options={breedsOptions}
+                  />
+                </Row>
+                <Row>
+                  <Select2
+                    name="port"
+                    title="Porte"
+                    placeholder="Selecione a categoria"
+                    options={['Pequeno', 'Médio', 'Grande']}
+                  />
+
+                  <Input2
+                    name="years_old"
+                    title="Idade"
+                    type="number"
+                    min="0"
+                    placeholder="Digite a idade do animal"
+                  />
+                </Row>
+                <Row>
+                  <Select2
+                    name="genre"
+                    title="Gênero"
+                    placeholder="Selecione o gênero"
+                    options={['Macho', 'Fêmea']}
+                  />
+                  <Input2
+                    name="name"
+                    title="Nome"
+                    placeholder="Digite o nome do animal"
+                  />
+                </Row>
+              </InputsContainer>
+
+              <Checkbox name="castrated" text="Animal castrado" />
+              <Checkbox name="pedigree" text="Animal possui pedigree" />
+
+              <Buttons>
+                <Button title="Proximo" type="submit" buttonType="next" />
+                <Button
+                  title="Voltar"
+                  buttonType="return"
+                  onClick={handlePreviusPage}
+                />
+              </Buttons>
+            </Page>
+
+            <Page visible={page === 3}>
+              <InputContainer>
+                <Input
+                  name="vaccines[0]"
+                  title="Vacinas já tomadas"
+                  placeholder="Vacina 1"
+                  icon={IoIosWater}
+                />
+              </InputContainer>
+              {vaccinesInputs.map((key, index) => (
+                <InputContainer key={key}>
+                  <Input
+                    name={`vaccines[${index + 1}]`}
+                    placeholder={`Vacina ${index + 2}`}
+                    icon={IoIosWater}
+                  />
+                  <button
+                    type="button"
+                    onClick={() => handleRemoveInput('vaccine', index)}
+                  >
+                    <IoIosClose size={36} />
+                  </button>
+                </InputContainer>
+              ))}
+              <AddButton
+                type="button"
+                onClick={() => handleAddInput('vaccine')}
+              >
+                <IoMdAdd size={32} />
+                <span>Adicionar uma nova vacina</span>
+              </AddButton>
+
+              <InputContainer>
+                <Input
+                  name="observations[0]"
+                  title="Observações"
+                  placeholder="Observações 1"
+                  icon={IoIosEye}
+                />
+              </InputContainer>
+
+              {observationsInputs.map((key, index) => (
+                <InputContainer key={key}>
+                  <Input
+                    name={`observations[${index + 1}]`}
+                    placeholder={`Observações ${index + 2}`}
+                    icon={IoIosEye}
+                  />
+                  <button
+                    type="button"
+                    onClick={() => handleRemoveInput('observation', index)}
+                  >
+                    <IoIosClose size={36} />
+                  </button>
+                </InputContainer>
+              ))}
+              <AddButton
+                type="button"
+                onClick={() => handleAddInput('observation')}
+              >
+                <IoMdAdd size={32} />
+                <span>Adicionar uma nova observação</span>
+              </AddButton>
+
+              <Buttons>
+                <Button title="Cadastrar" buttonType="confirm" />
+                <Button
+                  title="Voltar"
+                  buttonType="return"
+                  onClick={handlePreviusPage}
+                />
+              </Buttons>
+            </Page>
+          </Form>
+        </FormContainer>
+        {/* <Button title="Proximo" /> */}
+      </Content>
+    </Container>
+  );
+};
+
+export default NewAnimal;
---
+++ src/pages/NewAnimal/styles.js
@@ -0,0 +1,259 @@
+import styled, { css, keyframes } from 'styled-components';
+import { shade, lighten } from 'polished';
+
+import SelectInput from '../../components/Select';
+import TextInput from '../../components/Input';
+import ButtonInput from '../../components/Button';
+import CheckboxInput from '../../components/Checkbox';
+
+export const Container = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Content = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  width: 65vw;
+  margin: 0 auto;
+  border: 1px solid ${props => shade(0.1, props.theme.colors.background)};
+  border-top: 0px;
+  border-bottom: 0px;
+
+  @media (max-width: 1200px) {
+    width: 100vw;
+  }
+`;
+
+export const Title = styled.h1`
+  color: ${props => props.theme.colors.text.primary};
+  font-size: 2.5rem;
+  font-weight: bold;
+  text-align: center;
+  margin: 35px auto 15px auto;
+`;
+
+export const FormContainer = styled.div`
+  flex: 1;
+  width: 60%;
+  margin: 0 auto;
+
+  form {
+    height: 100%;
+  }
+
+  @media (max-width: 600px) {
+    width: 90vw;
+  }
+`;
+
+export const Select = styled(SelectInput)`
+  width: 100%;
+  h1 {
+    color: ${props => props.theme.colors.text.primary};
+  }
+`;
+
+export const Select2 = styled(Select)`
+  width: 48%;
+
+  h1 {
+    font-size: 1.8rem;
+  }
+
+  @media (max-width: 755px) {
+    width: 100%;
+    margin-bottom: 15px;
+  }
+`;
+
+export const Input = styled(TextInput)`
+  width: 100%;
+  h1 {
+    color: ${props => props.theme.colors.text.primary};
+  }
+`;
+
+export const Input2 = styled(Input)`
+  width: 48%;
+
+  h1 {
+    font-size: 1.8rem;
+  }
+
+  @media (max-width: 755px) {
+    width: 100%;
+  }
+`;
+
+export const FileInput = styled.div`
+  margin-top: 15px;
+`;
+
+export const Thumbnails = styled.div`
+  margin-top: 12px;
+`;
+
+export const PhotoInfo = styled.div`
+  display: flex;
+  align-items: center;
+  width: 80%;
+  background: ${props => props.theme.colors.text.secondary};
+  margin-top: 15px;
+  padding: 8px 10px;
+  border: 1px solid ${props => props.theme.colors.border};
+  border-radius: 5px;
+
+  div {
+    flex: 9;
+    display: flex;
+    align-items: center;
+    height: 25px;
+
+    img {
+      width: 45px;
+      max-height: 25px;
+    }
+
+    span {
+      margin-left: 3%;
+      font-size: 1.4rem;
+    }
+  }
+
+  button {
+    background: none;
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      color: ${props => props.theme.colors.error};
+      transition: color 0.2s;
+      &:hover {
+        color: ${props => shade(0.6, props.theme.colors.error)};
+      }
+    }
+  }
+`;
+
+export const Buttons = styled.div`
+  flex: 1;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  margin: 25px 0 80px 0;
+
+  button {
+    margin-top: 10px;
+  }
+
+  @media (max-width: 600px) {
+    width: 90vw;
+  }
+`;
+
+export const Button = styled(ButtonInput)`
+  width: 60%;
+`;
+
+const showingPage = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+export const Page = styled.div`
+  display: none;
+
+  animation: ${showingPage} 600ms;
+
+  ${props =>
+    props.visible &&
+    css`
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+    `}
+`;
+
+export const InputsContainer = styled.div`
+  max-width: 100%;
+  margin-bottom: 20px;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  width: 100%;
+
+  & + & {
+    margin-top: 15px;
+  }
+
+  @media (max-width: 755px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    & + & {
+      margin-top: 0;
+    }
+  }
+`;
+
+export const Checkbox = styled(CheckboxInput)`
+  label span {
+    color: ${props => props.theme.colors.text.primary};
+    font-size: 1.6rem;
+    font-weight: 400;
+  }
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+
+  & + & {
+    margin-top: 0;
+  }
+
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: none;
+    svg {
+      color: ${props => props.theme.colors.error};
+      margin-left: 5px;
+    }
+  }
+`;
+
+export const AddButton = styled.button`
+  display: flex;
+  align-items: center;
+  margin: 10px 0 0 15px;
+  color: ${props => lighten(0.15, props.theme.colors.text.primary)};
+  background: none;
+
+  svg {
+    color: ${props => lighten(0.15, props.theme.colors.text.primary)};
+  }
+
+  span {
+    margin-left: 5px;
+    font-size: 1.6rem;
+  }
+`;
