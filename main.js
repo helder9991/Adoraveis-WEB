a5671d5fe84aade21e553147d8395cd7a5f4ead9
Helder <helderafonsoss@hotmail.com>
2020-11-26 20:56:35 -0300

    Utilizacao do arquivo env para producao

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
+require('dotenv/config');
 const express = require('express');
+
 const app = express();
 
-const port = 3000;
+const baseDir = `${__dirname}/build/`;
+app.use(express.static(`${baseDir}`));
+app.get('/', (req, res) => res.sendfile('index.html', { root: baseDir }));
 
-const baseDir = `${__dirname}/build/`
-app.use(express.static(`${baseDir}`))
-app.get('/', (req, res) => res.sendfile('index.html' , { root : baseDir } ))
-
-app.listen(port)
+app.listen(process.env.REACT_PORT);
