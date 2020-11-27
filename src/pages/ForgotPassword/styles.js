8ca6c60ea6a743523e109e02666a4025c80de5ce
Helder <helderafonsoss@hotmail.com>
2020-11-26 22:19:31 -0300

    Remocao da margem a direita nos formularios

--- src/pages/ForgotPassword/styles.js
+++ src/pages/ForgotPassword/styles.js
@@ -69,7 +69,6 @@ export const FormContainer = styled.div`
   align-items: center;
   height: 50vh;
   width: 35%;
-  margin-right: 35px;
 
   form {
     display: flex;
--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -68,7 +68,6 @@ export const FormContainer = styled.div`
   align-items: center;
   height: 50vh;
   width: 35%;
-  margin-right: 35px;
 
   form {
     display: flex;
