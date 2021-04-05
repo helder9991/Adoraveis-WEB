cfe40887049a58355c4eeee9a90ca5d4a753db43
Helder Afonso <helderafonsoss@hotmail.com>
2021-04-05 18:11:37 -0300

    Atualizaçao no teste do Carousel

--- src/__tests__/components/Carousel.test.js
+++ src/__tests__/components/Carousel.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 
 import Carousel from '../../components/Carousel';
@@ -8,27 +8,27 @@ import Theme from '../../styles/themes/light';
 
 describe('Carousel Component', () => {
   it('should be able to render the carousel', () => {
-    const { getByAltText } = render(
+    render(
       <ThemeProvider theme={Theme}>
         <Carousel photos={['foto1', 'foto2', 'foto3']} />
       </ThemeProvider>,
     );
-
-    expect(getByAltText('Foto do animal')).toBeTruthy();
+    
+    expect(screen.getByAltText('Foto do animal')).toBeTruthy();
   });
 
   it('should be able to change current photo', () => {
-    const { getByTestId, getByAltText } = render(
+    render(
       <ThemeProvider theme={Theme}>
         <Carousel photos={['foto1.png', 'foto2.png', 'foto3.png']} />
       </ThemeProvider>,
     );
 
-    const buttonElement = getByTestId('button-container[1]');
+    const buttonElement = screen.getByTestId('button-container[1]');
 
     fireEvent.click(buttonElement);
 
-    expect(getByAltText('Foto do animal').getAttribute('src')).toBe(
+    expect(screen.getByAltText('Foto do animal').getAttribute('src')).toBe(
       'foto2.png',
     );
   });
